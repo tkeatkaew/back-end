c8ae3ab166ef74960815d01170d34336eda4c0aa
tkeatkaew <thanit@rmutl.ac.th`>
2024-02-07 16:03:10 +0700

    cors localhost:3001

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const init = async () => {
         routes: {
             //cors: true
             "cors": {
-                "origin": ["Access-Control-Allow-Origin", "192.168.179.111:3000"],
+                "origin": ["Access-Control-Allow-Origin", "localhost:3001"],
                 "headers": ["Accept", "Content-Type"],
                 "additionalHeaders": ["X-Requested-With"]
             }
